f21629a31ff5f45bb41277423f193c8175994573
AlifArifin <m.alif.arifin@gmail.com>
2018-04-08 16:17:17 +0700

    tes lala hehe yey

---
+++ Fish.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include "Fish.hpp"
+
+using namespace std;
+
+const static int hunger_time = 10;
+const static int dead_time = 20;
+const static int speed_fish = 8;
+
+Fish::Fish(string _j, int _v) : Summonable(_j, speed_fish), value(_v) {
+    hungry = false;
+    food_count = 0;
+    count_move = 0;
+    setChange_Move();
+}
+
+Fish::~Fish() {
+
+}
+
+/*Getter*/
+bool Fish::getHungry() const {
+    return hungry;
+}
+int Fish::getFood_Count() const {
+    return food_count;
+}
+
+static int getHunger_Time() {
+    return hunger_time;
+}
+
+static int getDead_Time() {
+    return dead_time;
+} 
+
+int Fish::getCount_Move() const {
+    return count_move;
+}
+
+int Fish::getChange_Move() const {
+    return change_move;
+}
+
+static int getSpeed_Fish() {
+    return speed_fish;
+}
+
+/*Setter*/
+void Fish::setHungry(bool _h) {
+    hungry = _h;
+}
+
+void Fish::setFood_Count(int _f) {
+    food_count = _f;
+}
+
+void Fish::setCount_Move(int _c) {
+    count_move = _c;
+}
+
+void Fish::setChange_Move() {
+    change_move = rand() % 45 + 5;
+}
---
+++ Fish.hpp
@@ -0,0 +1,46 @@
+#ifndef FISH_HPP
+#define FISH_HPP
+
+#include <iostream>
+#include "Summonable.hpp"
+#include "Coin.hpp"
+
+using namespace std;
+
+class Fish : public Summonable{
+    public:
+        /*Sekawan*/
+        Fish(string, int); //constructor jenis value
+        ~Fish(); //destructor dibuat untuk animasi mati biar dramatis
+        
+        /*Getter*/
+        bool getHungry() const;
+        int getFood_Count() const;
+        static int getHunger_Time();
+        static int getDead_Time(); 
+        int getCount_Move() const;
+        int getChange_Move() const;
+        int getValue() const;
+        static int getSpeed_Fish();
+
+        /*Setter*/
+        void setHungry(bool);
+        void setFood_Count(int);
+        void setCount_Move(int);
+        void setChange_Move(); //dilakukan secara random
+        
+        /*Method*/
+        virtual void eat() = 0; //untuk makan
+
+    protected:
+        bool hungry; //status kelaparan Fish
+        int food_count; //menghitung banyaknya makanan yang telah dimakan
+        const static int hunger_time; //periode Fish lapar
+        const static int dead_time; //waktu yang dibutuhkan 
+        int count_move; //untuk keperluan hunger_time dan dead_time dan coin (khusus guppy)
+        int change_move; //untuk keperluan penggantian arah (X Waktu)
+        static const int speed_fish; //mencatat kecepatan untuk Fish
+        const int value; //harga ikan
+};
+
+#endif
