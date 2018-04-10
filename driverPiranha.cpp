a8c1e6428c1ed296f438ef96b52c988db0502374
Pollycarpus <fijarmayoza.rabbi@gmail.com>
2018-04-11 03:04:47 +0700

    nambah driver

---
+++ driverPiranha.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Guppy.hpp"
+#include "Point.hpp"
+
+using namespace std;
+
+int main(){
+    Point p1(5, 5);
+    Piranha pr(p1);
+
+    int ph = g1.getValue_Piranha();
+    cout << "Piranha seharga : "<< ph << "Telah di-construct. "<< endl;
+    
+    return 0;
+}
