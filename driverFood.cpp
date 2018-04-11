5744d169af7cffdf9ac4cd25a917b8f7c3cf5321
Rahmat Santosa <rnsantosa@yahoo.com>
2018-04-11 22:09:53 +0700

    driverFOod

---
+++ driverFood.cpp
@@ -0,0 +1,35 @@
+#include "Food.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Point P1(150, 100);
+    Food C1(P1);
+
+    Matrix M;
+
+    cout << "Direction : " << C1.getDirection() << endl;
+    cout << "Position : " << C1.getPosition() << endl;
+    cout << "Speed : " << C1.getSpeed() << endl;
+    cout << "Radius : " << C1.getRadius_Food() << endl;
+    cout << "Image : " << C1.getImage() << endl;
+    cout << "Harga : " << C1.getValue_Food() << endl;
+
+    Food C2(P1);
+    cout << "Operator== : " << (C2 == C1) << endl;
+    
+    int time = 0;
+
+    while (time < 3) {
+        time++;
+        C1.move(M, time);
+        cout << "---- SETELAH MOVE " << time << " SEC ----" << endl;
+        cout << "Direction : " << C1.getDirection() << endl;
+        cout << "Position : " << C1.getPosition() << endl;
+        cout << "Speed : " << C1.getSpeed() << endl;
+        cout << "Radius : " << C1.getRadius_Food() << endl;
+        cout << "Image : " << C1.getImage() << endl;
+    }
+
+    return 0;
+}
